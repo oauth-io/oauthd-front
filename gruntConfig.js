fbd357c4a16dcfe38973ce8455a3050402aa2149
Vincent Lefebvre <vinc.lefebv@gmail.com>
2014-09-26 16:47:55 +0200

    update gitignore and gruntconfig

--- gruntConfig.js
+++ gruntConfig.js
@@ -2,6 +2,24 @@
 
 module.exports = function(gruntConf) {
 
+	gruntConf.coffee['default_front'] = {
+		expand: true,
+		cwd: __dirname,
+		src: ['*.coffee'],
+		dest: __dirname + '/bin',
+		ext: '.js',
+		options: {
+			bare: true
+		}
+	};
+
+	gruntConf.watch['default_front'] = {
+		files: [
+			__dirname + '/**/*.coffee'
+		],
+		tasks: ['coffee:default_front']
+	};
+
 	gruntConf.watch['front'] = {
 		files: [
 			__dirname + '/**/*.coffee',
