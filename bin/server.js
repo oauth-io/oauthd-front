8c782c5399d0ad11f65dec59d3ab81bcce1cabd1
Antoine Jackson <a.j.william26@gmail.com>
2014-09-28 17:13:17 +0200

    Added compile files

---
+++ bin/server.js
@@ -0,0 +1,33 @@
+var ecstatic, fs, restify;
+
+restify = require('restify');
+
+ecstatic = require('ecstatic');
+
+fs = require('fs');
+
+module.exports = function(env) {
+  var exp;
+  exp = {};
+  exp.setup = function(callback) {
+    this.server.get(/^(\/.*)/, function(req, res, next) {
+      return fs.stat(__dirname + '/public' + req.params[0], function(err, stat) {
+        if (stat != null ? stat.isFile() : void 0) {
+          next();
+        } else {
+          return fs.readFile(__dirname + '/public/index.html', {
+            encoding: 'UTF-8'
+          }, function(err, data) {
+            res.setHeader('Content-Type', 'text/html');
+            res.send(200, data);
+          });
+        }
+      });
+    }, restify.serveStatic({
+      directory: __dirname + '/public',
+      "default": __dirname + '/public/index.html'
+    }));
+    return callback();
+  };
+  return exp;
+};
